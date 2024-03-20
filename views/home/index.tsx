c5d5bfb75cd0d1ff6dbfe39983508e9de69f6962
git-marcopitra <marcomorete73@hotmail.com>
2024-03-20 18:02:08 +0100

    :sparkles: feat: add indicators side

--- views/home/index.tsx
+++ views/home/index.tsx
@@ -1,12 +1,17 @@
 import { Box, Typography } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
+import Indicators from './indicators';
+
 const Home: FC = () => (
-  <Box>
-    <Typography variant="display" size="large" color="onSurface">
-      Home
-    </Typography>
-  </Box>
+  <>
+    <Indicators />
+    <Box gridColumn={['1/-1', '1/-1', '6/-1']}>
+      <Typography size="large" variant="display" color="onSurface">
+        Home
+      </Typography>
+    </Box>
+  </>
 );
 
 export default Home;
